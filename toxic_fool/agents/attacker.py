c8c0c8f5a3fe07fb776d7e3d37dd76672341bdc0
orgoro <or.gorodissky@d-id.com>
2019-06-04 16:08:21 +0300

    pylint: mask error

--- toxic_fool/agents/attacker.py
+++ toxic_fool/agents/attacker.py
@@ -29,9 +29,8 @@ def create_token_dict(char_idx):
 
 
 class RandomFlip(object):
-    def attack(self, seq, mask, token_index, char_index):
+    def attack(self, seq, mask, token_index, char_index):  # pylint: disable=unused-argument
         assert len(mask) == len(seq)
-        char_index = None  # pylint: disable=unused-argument
         masked_seq = seq * mask
         spaces_indices = np.where(seq == SPACE_EMBEDDING)
         masked_seq[spaces_indices] = 0
